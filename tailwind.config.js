6fac611c2aad3d743017b1ae08adfd48f9d4788a
Sebastian <sebastian.kucera@icloud.com>
2023-03-26 23:21:40 +0200

    socials + projects update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       colors: {
         'toxicpurple' :  "#69369E",
         'navbar' : '#111111',
+        'steam' : '#4a6d9a',
+        'twitter' : '#00acee',
+        'discord' : '#7289da',
+        'twitch' : '#6441a4',
       }
     },
   },
