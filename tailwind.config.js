3395985e2bdb0d25690861b740a3b93d4c4c5036
Sebastian <sebastian.kucera@icloud.com>
2023-03-26 18:23:33 +0200

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'toxicpurple' :  "rgb(147 51 234)",
+      }
+    },
   },
   plugins: [],
 }
